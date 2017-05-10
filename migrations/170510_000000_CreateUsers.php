bb64c57b0c13a98cdc6e8780006c7413a5cd9d0e
Changwan Jun <im@wani.kr>
2017-05-10 16:57:11 +0900

    add Users

---
+++ migrations/170510_000000_CreateUsers.php
@@ -0,0 +1,34 @@
+<?php
+
+use Wandu\Database\Contracts\ConnectionInterface;
+use Wandu\Database\Migrator\Migration;
+use Wandu\Database\Query\CreateQuery;
+use Wandu\Database\Query\Expression\RawExpression;
+
+class CreateUsers extends Migration 
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function migrate(ConnectionInterface $connection)
+    {
+        $connection->query($connection->createQueryBuilder('users')->create(function (CreateQuery $table) {
+            $table->bigInteger('id')->unsigned()->autoIncrement();
+            $table->varchar("username", 20)->unique();
+            $table->varchar('password', 255)->nullable();
+            $table->json('permissions')->nullable();
+            $table->timestamp('created_at')->default(new RawExpression('CURRENT_TIMESTAMP'));
+            $table->addColumn(new RawExpression('`updated_at` TIMESTAMP DEFAULT now() ON UPDATE now()'));
+
+            $table->primaryKey('id');
+        }));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rollback(ConnectionInterface $connection)
+    {
+        $connection->query($connection->createQueryBuilder('users')->drop());
+    }
+}
